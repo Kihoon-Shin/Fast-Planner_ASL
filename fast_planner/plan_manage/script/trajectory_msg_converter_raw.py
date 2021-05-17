7bf8ee1873a055e200c68ee30ddca57084151377
beomsu7 <72853382+beomsu7@users.noreply.github.com>
2021-05-17 11:39:53 +0900

    Update trajectory_msg_converter_raw.py

--- fast_planner/plan_manage/script/trajectory_msg_converter_raw.py
+++ fast_planner/plan_manage/script/trajectory_msg_converter_raw.py
@@ -1,56 +1,50 @@
 #!/usr/bin/env python
 
-"""@trajectory_msg_converter.py
-This node converts Fast-Planner reference trajectory message to MultiDOFJointTrajectory which is accepted by geometric_controller
-origial file's Authors: Mohamed Abdelkader
-https://github.com/mzahana/px4_fast_planner
-reference : /px4_fast_planner/scripts/trajectory_msg_converter.py
-"""
-
-# Imports
 import rospy
-from trajectory_msgs.msg import MultiDOFJointTrajectory, MultiDOFJointTrajectoryPoint # for geometric_controller
 from quadrotor_msgs.msg import PositionCommand # for Fast-Planner
-from geometry_msgs.msg import Transform, Twist
-from tf.transformations import quaternion_from_euler
-from geometry_msgs.msg import PoseStamped
 from mavros_msgs.msg import PositionTarget
 
 class MessageConverter:
     def __init__(self):
         rospy.init_node('trajectory_msg_converter')
 
-        fast_planner_traj_topic = rospy.get_param('~fast_planner_traj_topic', 'planning/pos_cmd')
-        traj_pub_topic = rospy.get_param('~traj_pub_topic', 'mavros/setpoint_raw/local')
+        self.pose = PositionTarget()
+        self.pose.coordinate_frame = 1
+        self.pose.position.x = 0
+        self.pose.position.y = 0
+        self.pose.position.z = 1
+        self.pose.yaw = 0
 
-        # Publisher for geometric_controller
-        self.traj_pub = rospy.Publisher(traj_pub_topic, PositionTarget, queue_size=3)
+        self.traj_pub = rospy.Publisher('mavros/setpoint_raw/local', PositionTarget, queue_size=1)
+        rospy.Subscriber('planning/pos_cmd', PositionCommand, self.fastPlannerTrajCallback)
 
-        # Subscriber for Fast-Planner reference trajectory
-        rospy.Subscriber(fast_planner_traj_topic, PositionCommand, self.fastPlannerTrajCallback, tcp_nodelay=True)
-
-        rospy.spin()
 
     def fastPlannerTrajCallback(self, msg):
         # position and yaw
-        pose = PositionTarget()
-        pose.position.x = msg.position.x
-        pose.position.y = msg.position.y
-        pose.position.z = msg.position.z
         
-        pose.velocity.x = msg.velocity.x
-        pose.velocity.y = msg.velocity.y
-        pose.velocity.z = msg.velocity.z
+        self.pose.position.x = msg.position.x
+        self.pose.position.y = msg.position.y
+        self.pose.position.z = msg.position.z
+        
+        self.pose.velocity.x = msg.velocity.x
+        self.pose.velocity.y = msg.velocity.y
+        self.pose.velocity.z = msg.velocity.z
 
-        pose.acceleration_or_force.x = msg.acceleration.x
-        pose.acceleration_or_force.y = msg.acceleration.y
-        pose.acceleration_or_force.z = msg.acceleration.z
+        self.pose.acceleration_or_force.x = msg.acceleration.x
+        self.pose.acceleration_or_force.y = msg.acceleration.y
+        self.pose.acceleration_or_force.z = msg.acceleration.z
 
+    
 
-        pose.yaw = msg.yaw
+        self.pose.yaw = msg.yaw
 
-
-        self.traj_pub.publish(pose)
+    def run(self):
+        rate = rospy.Rate(30)
+        while not rospy.is_shutdown():
+            
+            self.traj_pub.publish(self.pose)
+            rate.sleep()
 
 if __name__ == '__main__':
     obj = MessageConverter()
+    obj.run()
